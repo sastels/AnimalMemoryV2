dc2a9a4236b3bdeaf4980639d43dbd44df9ea0b3
Steve Astels <sastels@gmail.com>
2020-11-01 16:12:11 -0500

    Initial Commit

---
+++ AnimalMemoryV2/AnimalMemoryV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalMemoryV2App.swift
+//  AnimalMemoryV2
+//
+//  Created by Stephen Astels on 2020-11-01.
+//
+
+import SwiftUI
+
+@main
+struct AnimalMemoryV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalMemoryV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalMemoryV2
+//
+//  Created by Stephen Astels on 2020-11-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
